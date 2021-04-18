3b457028f30f64e4001b2ef615c90daf61ecd411
Parthros <36551929+Parthros@users.noreply.github.com>
2021-04-17 20:21:12 -0500

    Initial commit

---
+++ Need for Speed Underground Launcher/NFSU_Launcher.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Need_for_Speed_Underground_Launcher
+{
+    partial class frmLauncher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLauncher));
+            this.btnLaunch = new System.Windows.Forms.Button();
+            this.lblPleaseWait = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLaunch
+            // 
+            this.btnLaunch.BackColor = System.Drawing.Color.Black;
+            this.btnLaunch.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnLaunch.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.btnLaunch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLaunch.Font = new System.Drawing.Font("RussellSquare", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLaunch.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(100)))), ((int)(((byte)(115)))));
+            this.btnLaunch.Location = new System.Drawing.Point(572, 484);
+            this.btnLaunch.Name = "btnLaunch";
+            this.btnLaunch.Size = new System.Drawing.Size(200, 65);
+            this.btnLaunch.TabIndex = 0;
+            this.btnLaunch.Text = "Play ►";
+            this.btnLaunch.UseVisualStyleBackColor = false;
+            this.btnLaunch.Click += new System.EventHandler(this.btnLaunch_Click);
+            // 
+            // lblPleaseWait
+            // 
+            this.lblPleaseWait.AutoSize = true;
+            this.lblPleaseWait.BackColor = System.Drawing.Color.Transparent;
+            this.lblPleaseWait.Font = new System.Drawing.Font("RussellSquare", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPleaseWait.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(100)))), ((int)(((byte)(115)))));
+            this.lblPleaseWait.Location = new System.Drawing.Point(12, 502);
+            this.lblPleaseWait.Name = "lblPleaseWait";
+            this.lblPleaseWait.Size = new System.Drawing.Size(452, 32);
+            this.lblPleaseWait.TabIndex = 1;
+            this.lblPleaseWait.Text = "Game launching, please wait...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("RussellSquare", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(100)))), ((int)(((byte)(115)))));
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(432, 14);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Need for Speed Underground Launcher Created by Patrick Brophy (2021)";
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.BackColor = System.Drawing.Color.Black;
+            this.btnAbout.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnAbout.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.btnAbout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAbout.Font = new System.Drawing.Font("RussellSquare", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbout.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(100)))), ((int)(((byte)(115)))));
+            this.btnAbout.Location = new System.Drawing.Point(702, 12);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(70, 23);
+            this.btnAbout.TabIndex = 3;
+            this.btnAbout.Text = "About";
+            this.btnAbout.UseVisualStyleBackColor = false;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // frmLauncher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblPleaseWait);
+            this.Controls.Add(this.btnLaunch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "frmLauncher";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Need for Speed Underground Launcher";
+            this.Load += new System.EventHandler(this.frmLauncher_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frmLauncher_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLaunch;
+        private System.Windows.Forms.Label lblPleaseWait;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAbout;
+    }
+}
+
---
+++ Need for Speed Underground Launcher/NFSU_Launcher.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Management.Automation;
+using System.Windows.Forms;
+using Need_for_Speed_Underground_Launcher.Properties;
+
+namespace Need_for_Speed_Underground_Launcher
+{
+    public partial class frmLauncher : Form
+    {
+        public frmLauncher()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLaunch_Click(object sender, EventArgs e)
+        {
+            #region Play Sound Effect
+            if(File.Exists(Settings.Default.PlayButton_Click_WAV))
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(Settings.Default.PlayButton_Click_WAV);
+                player.Play();
+            }
+            #endregion Play Sound Effect
+
+            lblPleaseWait.Visible = true;
+            btnLaunch.Enabled = false;
+            btnLaunch.BackColor = Color.LightGray;
+            this.Refresh();
+            LaunchNFSU();
+        }
+
+        private void LaunchNFSU()
+        {
+            try
+            {
+                string isoPath = Path.Combine(Settings.Default.NFSU_ISO_Path, Settings.Default.NFSU_ISO_Filename);
+                FileInfo NFSU_Game_exe = new FileInfo(Path.Combine(Settings.Default.NFSU_GameEXE_Path, Settings.Default.NFSUL_GameEXE_Filename));
+
+                #region Check if The ISO and Game Executable Exist
+                if(!File.Exists(isoPath))
+                    throw new FileNotFoundException($"Could not find the specified ISO file!  \r\nFile Specified: \"{isoPath}\"");
+                if(!NFSU_Game_exe.Exists)
+                    throw new FileNotFoundException($"Could not find the specified Game executable file!  \r\nFile Specified: \"{NFSU_Game_exe.FullName}\"");
+                #endregion Check if the ISO and Game Executable Exist
+
+                using(PowerShell ps = PowerShell.Create())
+                using(Process NFSU_Game = new Process())
+                {
+                    #region Mount Need for Speed Underground Disk 2 ISO
+                    if(!DetectISOMount())
+                    {
+                        ps.AddCommand("Mount-DiskImage").AddParameter("ImagePath", isoPath).Invoke();
+                        ps.Commands.Clear();
+                    }
+                    #endregion Mount Need for Speed Underground Disk 2 ISO
+
+                    #region Make Sure Need for Speed Underground isn't Already Running
+                    bool? continueAnyway = null;
+                    if(Process.GetProcessesByName(NFSU_Game_exe.Name.Replace(NFSU_Game_exe.Extension, "")).Length > 0)
+                        continueAnyway = MessageBox.Show("It appears that Need for Speed Underground is already running.  Do you want to launch the game again anyway?  This may cause unexpected behavior.  \r\n\r\nIf you select\"No\" the launcher will exit.", "Confirm Game Launch", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes;
+                    
+                    if(continueAnyway == false)
+                    {
+                        //Not dismounting the ISO, since Need for Speed was already running.
+                        //ResetForm();
+                        return;     //The finally { } block will still be executed
+                    }
+                    #endregion Make Sure Need for Speed Underground isn't Already Running
+
+                    #region Start Need for Speed Underground Game
+                    NFSU_Game.StartInfo = new ProcessStartInfo()
+                    {
+                        WorkingDirectory = Settings.Default.NFSU_GameEXE_Path,
+                        FileName = Settings.Default.NFSUL_GameEXE_Filename
+                    };
+                    NFSU_Game.Start();          //Start the game
+                    this.Hide();                //Hide the launcher so the user doesn't see it while the game is running
+                    NFSU_Game.WaitForExit();    //Waits for the user to stop playing the game
+                    //this.Show();              //Uncomment to unhide the launcher after the user exits the game
+                    #endregion Start Need for Speed Underground Game
+
+                    #region Dismount Need for Speed Underground Disk 2 ISO
+                    if(!File.Exists(isoPath))
+                        throw new FileNotFoundException($"The specified ISO file has gone missing!  \r\nFile Specified: \"{isoPath}\"");
+
+                    if(DetectISOMount())
+                        ps.AddCommand("Dismount-DiskImage").AddParameter("ImagePath", isoPath).Invoke();
+
+                    if(DetectISOMount())    //Whoops!  We didn't successfully dismount the ISO!
+                        throw new TypeUnloadedException($"Could not dismount the ISO!  To manually dismount, open \"This PC\" right click the {Settings.Default.NFSU_ISO_VolumeLabel} Drive, and select \"Eject\"");
+                    #endregion Dismount Need for Speed Underground Disk 2 ISO
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"An error was encountered!  Launcher will exit!  {Environment.NewLine}Exception Details (Try Googling this): {ex}", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                this.Close();   //Exit the launcher
+            }
+        }
+
+        private bool DetectISOMount()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach(DriveInfo drive in drives)
+                if(drive.IsReady && drive.VolumeLabel.Equals(Settings.Default.NFSU_ISO_VolumeLabel, StringComparison.InvariantCultureIgnoreCase))
+                    return true;
+            return false;
+        }
+
+        private void frmLauncher_Load(object sender, EventArgs e)
+        {
+            //Check to make sure there's not another instance of this launcher running
+            if(Process.GetProcessesByName(Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly().Location)).Length > 1)
+            {
+                MessageBox.Show("Another instance of the Need for Speed Underground Launcher is already running!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();   //Exit the launcher
+            }    
+
+            lblPleaseWait.Visible = false;
+
+            string currentDir = Directory.GetCurrentDirectory();
+            string startupOptionsFile = Path.Combine(currentDir, "StartupOptions.txt");
+
+            if(File.Exists(startupOptionsFile))
+            {
+                using(StreamReader sr = new StreamReader(startupOptionsFile))
+                {
+                    string line = "";
+                    while((line = sr.ReadLine()) != null)
+                    {
+                        if(string.IsNullOrWhiteSpace(line))
+                            continue;
+
+                        if(line.Contains("Skip Launcher", true) && line.Contains("True", true))
+                        {
+                            btnLaunch_Click(null, null);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void frmLauncher_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar.Equals(Keys.Enter))
+                btnLaunch_Click(null, null);
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            #region Play Sound Effect
+            if(File.Exists(Settings.Default.PlayButton_Click_WAV))
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(Settings.Default.PlayButton_Click_WAV);
+                player.Play();
+            }
+            #endregion Play Sound Effect
+
+            MessageBox.Show(
+                "Created by Patrick Brophy (2021)  \r\n\r\n\r\n\r\n" +
+                "Frequently Asked Questions:\r\n\r\n\r\n" +
+                "Q: Where should I put these launcher files?  \r\n\r\n" +
+                "A: The recommended location is to create a new \"Launcher\" folder inside Need for Speed Underground's installed location.  Then, put a shortcut to \"Need for Speed Underground Launcher.exe\" on your desktop.  \r\n\r\n\r\n" +
+
+                "Q: The launcher keeps saying it can't find the ISO or EXE file.  How can I fix that?  \r\n\r\n" +
+                $"A: Open \"{Path.Combine(Directory.GetCurrentDirectory(), "Need for Speed Underground Launcher.exe.config")}\" and update the paths to the installed locations of your ISO & game executable files, respectively.  \r\n\r\n\r\n" +
+
+                "Q: How can I skip the launcher, and directly start the game?  \r\n\r\n" +
+                $"A: Open \"{Directory.GetCurrentDirectory()}\" and create a new Text Document named \"StartupOptions.txt\".  Put a line in that file that says \"Skip Launcher:True\" and save the file.  " +
+                "Next time you open the Need for Speed Underground lancher, the game will start immediately, skipping the splash screen.",
+
+                "About Need for Speed Underground Launcher", MessageBoxButtons.OK, MessageBoxIcon.None);
+        }
+
+        /*
+        private void ResetForm()
+        {
+            lblPleaseWait.Visible = false;
+            btnLaunch.Enabled = true;
+            btnLaunch.BackColor = Color.Black;
+            this.Refresh();
+        }
+        */
+    }
+
+    public static class ExtensionMethods
+    {
+        public static bool Contains(this string text, string value, bool ignoreCase)
+        {
+            if(ignoreCase)
+                return text.ToUpper().Contains(value.ToUpper());
+            return text.Contains(value);
+        }
+
+        public static bool StartsWith(this string text, string value, bool ignoreCase)
+        {
+            if(ignoreCase)
+                return text.ToUpper().StartsWith(value.ToUpper());
+            return text.StartsWith(value);
+        }
+
+        /*
+        public static string Replace(this string text, string oldString, string newString, bool ignoreCase)
+        {
+            if(ignoreCase)
+            {
+                //TODO: Write the logic to do a case-insensitive replace
+            }
+            return text.Replace(oldString, newString);
+        }
+        */
+    }
+}
